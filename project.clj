42c5478b008c5a95ff9c2bdb6897b4e4a43513d4
Matti Ahinko <matti.ahinko@gofore.com>
2017-06-12 20:35:14 +0300

    Tweak logging

--- project.clj
+++ project.clj
@@ -7,8 +7,14 @@
                 [adamwynne/feedparser-clj "0.5.2"]
                 [twitter-api "1.8.0"]
                 [org.clojure/tools.logging "0.4.0"]
-                [org.slf4j/slf4j-log4j12 "1.7.25"]
+                [org.slf4j/slf4j-log4j12 "1.7.25" :exclusions
+                                                  [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jdmk/jmxtools
+                                                    com.sun.jmx/jmxri]]
                 [environ "1.1.0"]]
   :main ^:skip-aot stock-twitter-bot.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+            :dev {:resource-paths ["resources/dev"]}
+            :test {:resource-paths ["resources/dev"]}})
